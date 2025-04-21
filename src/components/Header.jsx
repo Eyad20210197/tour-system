10dc2e289fa566497fd6fb0969d88086a0c70291
robaa18 <robaahmed722@gmail.com>
2025-04-21 18:28:36 +0200

    Modified gitignore

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar"
-export default function Header() {
-  return (
-    <header className="head">
-        <h1>Kemet Wonders</h1>
-        <Navbar/>
-    </header>
-  )
-}
